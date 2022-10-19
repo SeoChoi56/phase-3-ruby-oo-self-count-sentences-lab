aa792f7d8b2602210a6e9ce5d7a98b94129e0a36
Seo Choi <seochoi56@gmail.com>
2022-10-18 22:21:23 -0400

    commit tree d53a67ce8fbf1c1d8d3ecc2abc4354da13962afa

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self[-1] == "." ? true : false
   end
 
   def question?
-
+    return self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    return self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    splitArray = self.split(/[\!\?\.]/)
+    if splitArray.include?("")
+      splitArray.delete("")
+    end
+    return splitArray.length
   end
 end
